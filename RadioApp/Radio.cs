13e07a9a30f64d49ce3612fcaa6e668fb4357c43
Muhammed Miah <miahsuleman14@gmail.com>
2020-09-21 13:51:23 +0100

    Add project files.

---
+++ RadioApp/Radio.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RadioApp
+{
+    public class Radio
+    {
+        // implement a class Radio that corresponds to the Class diagram 
+        //   and specification in the Radio_Mini_Project document
+        protected int _channel =1;
+        private bool _on;
+
+        public int Channel {
+            get { return _channel;}
+            set
+            {
+                if (_on == true)
+                {
+                    if (value < 1 || value > 4)
+                    {
+
+                    }
+                    else
+                    {
+                        // Channel = value;
+                        _channel = value;
+                    }
+                }
+            }
+        }
+
+        public Radio()
+        {
+            //_channel = 1;
+            _on = false;
+        }
+        public string Play()
+        {
+            if(_on == true)
+            {
+               // Channel = 4;
+                return $"Playing channel {_channel}";
+            }
+            else
+            {
+                return $"Radio is off";
+            }
+        }
+
+        public void TurnOff()
+        {
+            if( _on == true)
+            {
+                _on = false;
+                _channel = 1;
+            }
+            // throw new NotImplementedException();
+        }
+
+        public void TurnOn()
+        {
+                _on = true;
+
+        }
+    }
+}
